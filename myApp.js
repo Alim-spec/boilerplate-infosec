8d9e860bdef0ff9cd1455a9c55d42ac418495b67
Alim Almambetov <alim.almambetov@gmail.com>
2025-02-27 08:49:56 +0000

    commit

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
+
 app.use(helmet.frameguard({ action: 'deny' }));
 
-
+app.use(helmet.hidePoweredBy());
 
 
 
